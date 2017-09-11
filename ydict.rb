0d833188f036bb45836971f87bbb0c6c0dfb4ed9
Timothy Ye <yexiaozhou2003@gmail.com>
2017-09-11 12:26:30 +0800

    Update ydict.rb

--- ydict.rb
+++ ydict.rb
@@ -1,8 +1,8 @@
 class Ydict < Formula
   desc "Yet another command-line dictionary for geeks!"
   homepage "https://github.com/TimothyYe/ydict"
-  url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-0.7.tar.gz"
-  sha256 "bfe7eadf248c2672f24156da1093288f811be473cff00b21e11a0e800a502c56"
+  url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-0.8.tar.gz"
+  sha256 "791dd0372b6b4d05fa145fae4f466368518e6815cba4c3151dd573c706fab8b5"
 
   def install
     bin.install "ydict"
