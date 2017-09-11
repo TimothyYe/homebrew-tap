5a78d013e433001cc0beddf2930de9c816ab1183
Timothy Ye <yexiaozhou2003@gmail.com>
2017-09-11 17:19:04 +0800

    Update ydict.rb

--- ydict.rb
+++ ydict.rb
@@ -3,7 +3,8 @@ class Ydict < Formula
   homepage "https://github.com/TimothyYe/ydict"
   url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-0.8.tar.gz"
   sha256 "791dd0372b6b4d05fa145fae4f466368518e6815cba4c3151dd573c706fab8b5"
-
+  
+  depends_on :mpg321
   def install
     bin.install "ydict"
   end
