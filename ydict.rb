678244785df8c1117ce4e36d3f3d085842008084
Timothy <yexiaozhou2003@gmail.com>
2017-10-19 16:00:27 +0800

    update ydict to V1.1

--- ydict.rb
+++ ydict.rb
@@ -1,8 +1,8 @@
 class Ydict < Formula
   desc "Yet another command-line dictionary for geeks!"
   homepage "https://github.com/TimothyYe/ydict"
-  url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-1.0.tar.gz"
-  sha256 "80f118dd2e8dc52ce0cb5b20bd9462bc3fc057f5bb61981ce04fe105880039a8"
+  url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-1.1.tar.gz"
+  sha256 "57201a43fbbfaae90912f56df1ecc2f193266a77609c4c0f90cf047f3d938634"
   
   depends_on "mpg123" => :run
   
