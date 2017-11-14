a8aa41ecbf309b7602ce50dc97e2aa4378287d98
Timothy <yexiaozhou2003@gmail.com>
2017-11-14 15:50:39 +0800

    for V0.3.2 release

--- skm.rb
+++ skm.rb
@@ -1,8 +1,8 @@
 class Skm < Formula
   desc "A simple and powerful SSH Keys Manager"
   homepage "https://github.com/TimothyYe/skm"
-	url "http://ow3g6s6og.bkt.clouddn.com/skm-mac64-0.3.1.tar.gz"
-  sha256 "bfba1e1a9b4c9515928daaf31c2e48c1c20f91934f9c1343f14f76c70aa8376a"
+	url "http://ow3g6s6og.bkt.clouddn.com/skm-mac64-0.3.2.tar.gz"
+  sha256 "97302b2e134641ca4dd43c72bebb745bb3fdcc7c6cbc2d19138620633303fcc6"
   
   def install
     bin.install "skm"
