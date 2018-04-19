642518ffab1d5b22b66cab39d52110d65d573365
Timothy <yexiaozhou2003@gmail.com>
2018-04-19 13:38:35 +0800

    update ydict tap file

--- ydict.rb
+++ ydict.rb
@@ -4,8 +4,6 @@ class Ydict < Formula
 	url "http://ow3g6s6og.bkt.clouddn.com/ydict-mac64-1.2.tar.gz"
   sha256 "42269542ec6e92796dc5784dc2b6f0efbd00a257dfb2f45a668bebb1aaad54af"
   
-  depends_on "mpg123" => :run
-  
   def install
     bin.install "ydict"
   end
