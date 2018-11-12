2cd73bcb9636bbb9461726dc93847d7a02916e6e
Timothy <yexiaozhou2003@gmail.com>
2018-11-12 23:19:14 +0800

    update hash for exchangerate

--- exchangerate.rb
+++ exchangerate.rb
@@ -2,7 +2,7 @@ class Exchangerate < Formula
   desc "A command-line tool to query exchange rate"
   homepage "https://github.com/TimothyYe/exchangerate"
 	url "https://download.xiaozhou.xyz/er-mac64-1.0.tar.gz"
-  sha256 "0146c710ded45b986545448049b48ba7688b8dc22adfaaf1d2db2773a827b2bd"
+  sha256 "3026e77153f01d1994bd78be269b074e38080970ff64bb88a469b8b3ec604062"
   
   def install
     bin.install "er"
