e27ae37b2b167cd1d1ba1d4771d15ed2a8edac5d
TimothyYe <yexiaozhou2003@gmail.com>
2019-01-07 19:57:50 +0800

    release new version of exchangerate

--- exchangerate.rb
+++ exchangerate.rb
@@ -1,8 +1,8 @@
 class Exchangerate < Formula
   desc "A command-line tool to query exchange rate"
   homepage "https://github.com/TimothyYe/exchangerate"
-	url "https://download.xiaozhou.xyz/er-mac64-1.0.tar.gz"
-  sha256 "3026e77153f01d1994bd78be269b074e38080970ff64bb88a469b8b3ec604062"
+	url "https://download.xiaozhou.xyz/er-mac64-1.1.tar.gz"
+  sha256 "e6acd0a81bbc23b824e83199655a7551ea10bf8ae141a88bc9c97b302172b932"
   
   def install
     bin.install "er"
