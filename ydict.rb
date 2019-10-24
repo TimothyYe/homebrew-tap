446edb2065662f6ab5465d9d4a91118cfd5169d7
Timothy <yexiaozhou2003@gmail.com>
2019-10-24 14:32:31 +0800

    ydict V2.0

--- ydict.rb
+++ ydict.rb
@@ -1,8 +1,8 @@
 class Ydict < Formula
   desc "Yet another command-line dictionary for geeks!"
   homepage "https://github.com/TimothyYe/ydict"
-	url "https://download.xiaozhou.xyz/ydict-mac64-1.4.tar.gz"
-  sha256 "f1dde31d76c7c6a947a0bca246694e7aff087c34c6a5a673649d6d8d07e68312"
+	url "https://download.xiaozhou.xyz/ydict-mac64-2.0.tar.gz"
+  sha256 "a3146e306bd442374a3398cb62fd15919e146d7f6a3852c4e9260425f60d21d5"
   
   def install
     bin.install "ydict"
