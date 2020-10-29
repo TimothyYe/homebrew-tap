04c8308ffdc4450e08088321ed639ce16366b7bc
Timothy <yexiaozhou2003@gmail.com>
2020-10-29 18:55:48 +0800

    update exchagnerate to v1.3

--- exchangerate.rb
+++ exchangerate.rb
@@ -1,8 +1,8 @@
 class Exchangerate < Formula
   desc "A command-line tool to query exchange rate"
   homepage "https://github.com/TimothyYe/exchangerate"
-	url "https://download.xiaozhou.xyz/er-mac64-1.2.tar.gz"
-  sha256 "30e721cae5d72d2e002899f0be77c8952b57567cc44247ed4f7e31f90600f652"
+	url "https://download.xiaozhou.xyz/er-mac64-1.3.tar.gz"
+  sha256 "d0068dc2e728c737004b442b50b5c5435155ffa2a6cfd7799f3e2695d47e2930"
   
   def install
     bin.install "er"
