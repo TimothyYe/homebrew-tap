19380ebfeabaccb9e4bccbf58ff54430c50a3023
Timothy <yexiaozhou2003@gmail.com>
2021-04-27 00:10:27 +0800

    update skm

--- skm.rb
+++ skm.rb
@@ -1,8 +1,8 @@
 class Skm < Formula
   desc "A simple and powerful SSH Keys Manager"
   homepage "https://github.com/TimothyYe/skm"
-  url "https://download.xiaozhou.xyz/skm-mac64-0.8.1.tar.gz"
-  sha256 "381ad3609bce66aba8d0830758f6efa632c52a13d732e0adfbb1eba04de79027"
+  url "https://download.xiaozhou.xyz/skm-mac64-0.8.2.tar.gz"
+  sha256 "54fcf7f4327ae6ce1e4dc0322a925319ffbd14fdd53b9b8f86375a86236315c7"
   
   def install
     bin.install "skm"
