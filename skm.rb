91d30d547765e1810b4d44c03947037c0f80f080
Timothy <yexiaozhou2003@gmail.com>
2024-06-29 12:13:56 +0800

    update skm

--- skm.rb
+++ skm.rb
@@ -9,7 +9,7 @@ class Skm < Formula
     sha256 "1983c90b5710396a6b35f06bac0f7a69d5732e95bcea5a114f42b390eda3c4cd"
   elsif Hardware::CPU.arm?
     url "https://download.xiaozhou.xyz/skm-mac-arm64-0.8.6.tar.gz"
-    sha256 "97418b42fc3ca6d5a04f7314feb907501da5d15774e02457befebafdd8d1b740"
+    sha256 "5a34aecf714e7cfe276cc0378db5f4b00f18c143ef715b427b616a77e7e7b6af"
   end
 
   def install
